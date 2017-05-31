e74744246542e7f5918f9176fa2619dc7b8265ee
Santiago Gil <gil.educaciontdf@gmail.com>
2017-05-31 11:43:21 -0300

    HotFix

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ var http = require('http');
 var ecstatic = require('ecstatic');
 http.createServer(
   ecstatic({ root: __dirname + '/public/'})
-).listen(80);
- 
+).listen(8090); 
 console.log('Listening on :80');
