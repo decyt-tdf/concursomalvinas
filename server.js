954ed7f70f0f0ac439b9f7e868693be9d3f30494
Sheridan <sherix88@gmail.com>
2017-06-14 09:06:48 -0300

    edicion 2017

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var ecstatic = require('ecstatic');
 http.createServer(
   ecstatic({ root: __dirname + '/public/'})
-).listen(8090); 
+).listen(8089); 
 console.log('Listening on :80');
