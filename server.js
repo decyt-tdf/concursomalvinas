1bab2f2ad40afc5d569f17c871a373ca12de9dce
Sheridan <sherix88@gmail.com>
2018-01-23 12:56:20 -0300

    fix server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var ecstatic = require('ecstatic');
 http.createServer(
   ecstatic({ root: __dirname + '/public/'})
-).listen(8089); 
-console.log('Listening on :8089');
+).listen(80); 
+console.log('Listening on :80');
